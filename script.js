1567befdb478b08e12b33259aeaf884953270de4
aleixalynn <143188376+aleixalynn@users.noreply.github.com>
2024-03-05 10:37:57 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function disPlay(){
+    alert("You will be redirected to the next page");
+    var nextPage = window.open("second.html");
+}
+
+function changepic(){
+    image.src = 'images/pic1.jpg';
+}
+
+function previous(){
+    image.src = 'images/pic2.jpg'
+}
+ 
